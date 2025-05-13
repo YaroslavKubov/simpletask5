63552eddc0529ae2ebaf73b7b2f2cbe69a017f24
YaroslavKubov <yaroslavkubov@gmail.com>
2025-05-13 03:18:35 +0300

    Добавьте файлы проекта.

---
+++ simpletask5/simpletask5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    const double pi = 3.14;
+    double r;
+
+    std::cout << "enter R: ";
+    std::cin >> r;
+
+    double S = pi * r * 2;
+    double H = pi * r * r;
+
+    std::cout << fixed << setprecision(2);
+   
+    std::cout << "S = " << S << endl;
+    std::cout << "H = " << H;
+
+
+    return 0;
+}
